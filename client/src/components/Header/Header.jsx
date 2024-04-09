c60d76819a844d7a22ff2e485b18317327e312cc
Ivan <ivanwong810520@gmail.com>
2024-04-09 17:11:36 +0800

    Added social media link in home header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,3 +1,6 @@
+import { NavLink } from "react-router-dom";
+import { PiLinkedinLogoLight, PiGithubLogoLight } from "react-icons/pi";
+
 export function Header() {
     return (
         <header className="bg-primary-200 mb-24">
@@ -13,8 +16,25 @@ export function Header() {
                         </h1>
                         <h4 className="w-4/5">
                             I am a full-stack software engineer focused on
-                            developing reliable and scalable system, software, and web application.
+                            developing reliable and scalable system, software,
+                            and web application.
                         </h4>
+                        <div className="flex flex-row gap-3 text-4xl">
+                            <div className="hover:text-primary-300">
+                                <NavLink
+                                    to={
+                                        "https://www.linkedin.com/in/yitmengwong/"
+                                    }
+                                >
+                                    <PiLinkedinLogoLight />
+                                </NavLink>
+                            </div>
+                            <div className="hover:text-primary-300">
+                                <NavLink to={"https://github.com/yitmeng00"}>
+                                    <PiGithubLogoLight />
+                                </NavLink>
+                            </div>
+                        </div>
                     </div>
                     <div className="hidden lg:block">
                         <img
