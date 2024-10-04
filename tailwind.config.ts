23596306542a2f855e320e62f41a2d696ea8b1a4
Ivan Wong <85393037+yitmeng00@users.noreply.github.com>
2024-10-04 23:50:22 +0800

    Install sass preprocessor, create global styling and add favicon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,22 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./styles/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ["var(--font-poppins)"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          100: "#F4EEE8",
+          200: "#F2E6D9",
+          300: "#E1C8AB",
+          400: "#C1A788",
+          500: "#5F554A",
+          600: "#504538",
+        },
       },
     },
   },
