7ee3fd5057e3eaf42e6775e92fc5621d45c26da2
Ivan Wong <85393037+yitmeng00@users.noreply.github.com>
2024-10-25 19:59:57 +0800

    Refactor page structure
    Remove about page and move all about info to home page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import PageContainer from "@/ui/components/PageContainer";
+import { About, Skill, Project } from "@/ui/features/about";
 import { HomeHeader } from "@/ui/features/home";
 
 const HomePage = () => {
-  return <HomeHeader />;
+  return (
+    <>
+      <HomeHeader />
+      <PageContainer>
+        <About />
+        <Skill />
+        <Project />
+      </PageContainer>
+    </>
+  );
 };
 
 export default HomePage;
