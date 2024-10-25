923420ffa1a8c55d011590e13615d6bbba413dc5
Ivan Wong <85393037+yitmeng00@users.noreply.github.com>
2024-10-26 02:12:54 +0800

    Fix unescaped single quotes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,9 @@ const NotFound = () => {
     <section className="not-found__container">
       <SentimentVeryDissatisfiedIcon fontSize="inherit" />
       <h2 className="not-found__main-heading">Page Not Found</h2>
-      <h6 className="not-found__sub-heading">We can't seem to find the page you're looking for.</h6>
+      <h6 className="not-found__sub-heading">
+        We can&apos;t seem to find the page you&apos;re looking for.
+      </h6>
     </section>
   );
 };
