a24884f86982b7c0f67d4cad00f3dbae47d2c374
Ivan Wong <85393037+yitmeng00@users.noreply.github.com>
2024-11-01 16:46:47 +0800

    Update header heading and about data

--- ui/features/about/About.tsx
+++ ui/features/about/About.tsx
@@ -4,12 +4,11 @@ const About: React.FC = () => {
   return (
     <section className="about__container">
       <p className="about__paragraph-item">
-        Hey there! I’m a full-stack developer with a huge appetite for learning, currently focused
-        on full-stack web dev. For me, the best part of being a developer is being able to craft
-        solutions that actually make a difference. Whether it’s developing a feature that improves
-        user experience or building something from scratch that solves a real-life problem, it’s all
-        about using tech as a modern-day magic wand to make life easier, more efficient, and more
-        convenient.
+        Hey there! Exploring and learning new things has been my passion forever. For me, the best
+        part of being a developer is being able to craft solutions that actually make a difference.
+        Whether it’s developing a feature that improves user experience or building something from
+        scratch that solves a real-life problem, it’s all about using tech as a modern-day magic
+        wand to make life easier, more efficient, and more convenient.
       </p>
       <p className="about__paragraph-item">
         When I’m not coding, you’ll likely find me with a camera in hand.{" "}
