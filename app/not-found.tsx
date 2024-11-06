b82b0795beb2b3e08d4b2de21f1d600fda7b4d65
Ivan Wong <85393037+yitmeng00@users.noreply.github.com>
2024-11-07 01:33:57 +0800

    Modify skill and refactor components styling and structure

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDiss
 const NotFound = () => {
   return (
     <section className="not-found__container">
-      <SentimentVeryDissatisfiedIcon fontSize="inherit" />
+      <SentimentVeryDissatisfiedIcon sx={{ color: "#504538", fontSize: 72 }} />
       <h1 className="not-found__main-heading">Page Not Found</h1>
       <h6 className="not-found__sub-heading">
         We can&apos;t seem to find the page you&apos;re looking for.
--- app/portfolio/layout.tsx
+++ app/portfolio/layout.tsx
@@ -2,10 +2,14 @@
 
 import { Footer } from "@/ui/components/layout";
 import { ReactNode } from "react";
+import { motion, useScroll } from "framer-motion";
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
+  const { scrollYProgress } = useScroll();
+
   return (
     <>
+      <motion.div className="main__progress-bar" style={{ scaleX: scrollYProgress }} />
       {children}
       <Footer />
     </>
