0d3930cef250cb5bb58fc7b68ae61a3cbcf50c4c
Ivan Wong <85393037+yitmeng00@users.noreply.github.com>
2024-11-07 01:30:47 +0800

    Create components needed for project detail page

---
+++ ui/components/MainChip.tsx
@@ -0,0 +1,31 @@
+import { Chip } from "@mui/material";
+
+interface MainChipProps {
+  techStack: string[];
+}
+
+const MainChip: React.FC<MainChipProps> = ({ techStack }) => {
+  return (
+    <div className="skill__wrapper">
+      <div className="skill__chip-wrapper">
+        {techStack.map((tech, index) => (
+          <div key={index}>
+            <Chip
+              label={tech}
+              variant="outlined"
+              sx={{
+                borderColor: "#504538",
+                fontFamily: "var(--font-noto-serif), sans-serif",
+                color: "#504538",
+                fontWeight: 600,
+                boxShadow: "rgba(95,85,74,0.75) 1.5px 1.5px 3px",
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MainChip;
