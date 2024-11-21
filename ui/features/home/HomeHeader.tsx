336c436d3f140e8bd4065f10c9848c3247e4e0b0
Ivan Wong <85393037+yitmeng00@users.noreply.github.com>
2024-11-22 00:11:33 +0800

    Update home header styling and apply hidden for scrollbar

--- ui/features/home/HomeHeader.tsx
+++ ui/features/home/HomeHeader.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { motion } from "motion/react";
 
 const HomeHeader: React.FC = () => {
   return (
@@ -13,26 +14,16 @@ const HomeHeader: React.FC = () => {
           I’m a full-stack developer based in Kuala Lumpur, focused on full-stack web development.
         </p>
         <div className="header__icon-wrapper">
-          <Link href={"https://www.linkedin.com/in/yitmengwong/"}>
-            <LinkedInIcon
-              fontSize="large"
-              sx={{
-                ":hover": {
-                  color: "#504538",
-                },
-              }}
-            />
-          </Link>
-          <Link href={"https://github.com/yitmeng00"}>
-            <GitHubIcon
-              fontSize="large"
-              sx={{
-                ":hover": {
-                  color: "#504538",
-                },
-              }}
-            />
-          </Link>
+          <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.95 }}>
+            <Link href={"https://www.linkedin.com/in/yitmengwong/"}>
+              <LinkedInIcon fontSize="large" />
+            </Link>
+          </motion.div>
+          <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.95 }}>
+            <Link href={"https://github.com/yitmeng00"}>
+              <GitHubIcon fontSize="large" />
+            </Link>
+          </motion.div>
         </div>
       </div>
       <img
