1d628e7e62784abc2ac12fe16ae86affbfa8b46c
Ivan Wong <ivanwong810520@gmail.com>
2026-04-16 00:06:51 +0800

    Update bio

--- shared/data/bio.ts
+++ shared/data/bio.ts
@@ -29,47 +29,42 @@ export const BIO_DATA: Bio[] = [
   },
   {
     id: 6,
-    year: "2020/10",
-    desc: "Participated in MYHackathon.",
-  },
-  {
-    id: 7,
     year: "2021/09 - 2021/12",
     desc: "Full-Stack Developer Intern at W. I. T. Solution.",
   },
   {
-    id: 8,
+    id: 7,
     year: "2022/01 - 2022/04",
     desc: "Web Application Developer Intern at IBM.",
   },
   {
-    id: 9,
+    id: 8,
     year: "2022/03",
     desc: `Obtained IBM <a href="https://www.credly.com/badges/4ebf470a-099e-45a9-b479-c053c0087788/public_url" target="_blank"><u>Enterprise Design Thinking Practitioner</u></a> certification.`,
   },
   {
-    id: 10,
+    id: 9,
     year: "2019/05 - 2022/04",
     desc: "Completed a Bacehlor's Degree in Computer Science at UCSI University.",
   },
   {
-    id: 11,
+    id: 10,
     year: "2022",
     desc: `Published <i><a href="https://www.jstage.jst.go.jp/article/jaalr/3/2/3_7/_article" target="_blank"><u>"Go.Travel - A Smart Tourism Guide Mobile Application"</i></u></a> in the Journal of Advances in Artificial Life Robotics.`,
   },
 
   {
-    id: 12,
+    id: 11,
     year: "2022/05 - 2024/06",
     desc: "Full Stack Software Engineer at Aspen Spectra.",
   },
   {
-    id: 13,
+    id: 12,
     year: "2024/06 - 2026/02",
     desc: "Software Engineer at Snappymob.",
   },
   {
-    id: 14,
+    id: 13,
     year: "2026/03",
     desc: `Obtained <a href="https://www.credly.com/badges/424f6aaa-740b-4f74-831e-7a2b55c31bc7/public_url" target="_blank"><u>AWS Certified Solutions Architect - Associate</u></a> certification.`,
   },
