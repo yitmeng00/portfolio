3a33d6209c4da06665a227d3624a3c5625258862
Ivan Wong <ivanwong810520@gmail.com>
2026-04-15 21:43:00 +0800

    Add skeleton for loading image

--- ui/features/blog/BlogCard.tsx
+++ ui/features/blog/BlogCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 import { BlogPost } from "@/shared/interface/Blog";
 
@@ -8,16 +9,21 @@ interface BlogCardProps {
 }
 
 const BlogCard = ({ post }: BlogCardProps) => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div id="blog-card" className="group">
       <Link href={`/blog/${post.slug}`}>
         <div className="blog-card__image-wrapper">
+          {isLoading && <div className="absolute inset-0 animate-pulse bg-gray-200" />}
           <Image
             src={post.thumbnailUrl}
             alt={post.title}
             loading="eager"
             fill
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            onLoadingComplete={() => setIsLoading(false)}
+            className={`${isLoading ? "opacity-0" : "opacity-100"}`}
           />
         </div>
         <span className="blog-grid-category">{post.category}</span>
