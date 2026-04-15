f1a1ddf60de80e088b98beb8ec48efd2a5274488
Ivan Wong <ivanwong810520@gmail.com>
2026-04-15 10:08:08 +0800

    Refactor navbar component
    Replace navbar animation with pure css instead of motion, update navbar component styling

--- ui/components/MainMenu.tsx
+++ ui/components/MainMenu.tsx
@@ -1,34 +1,13 @@
-import { motion } from "motion/react";
 import Link from "next/link";
 
 import SocialLink from "./SocialLink";
-
-const navbar = {
-  open: (height: number = 1000) => ({
-    clipPath: `circle(${height * 2 + 200}px at calc(100% - 50px) 50px)`,
-    transition: {
-      type: "spring" as const,
-      stiffness: 20,
-      restDelta: 2,
-    },
-  }),
-  closed: {
-    clipPath: "circle(30px at calc(100% - 50px) 50px)",
-    transition: {
-      delay: 0.2,
-      type: "spring" as const,
-      stiffness: 400,
-      damping: 40,
-    },
-  },
-};
 
 interface MainMenuProps {
   onToggleOpen: () => void;
 }
 
 const MainMenu: React.FC<MainMenuProps> = ({ onToggleOpen }) => (
-  <motion.div initial={false} variants={navbar} className="navbar__container">
+  <div className="navbar__container">
     <div className="navbar__menu-wrapper">
       <Link href="/" className="navbar__menu-item" onClick={onToggleOpen}>
         <h1>Home</h1>
@@ -38,7 +17,7 @@ const MainMenu: React.FC<MainMenuProps> = ({ onToggleOpen }) => (
       </Link>
       <SocialLink />
     </div>
-  </motion.div>
+  </div>
 );
 
 export default MainMenu;
