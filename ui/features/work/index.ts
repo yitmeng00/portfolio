de344379e3bc2ff9bdeb2893ca753436c708d6c7
Ivan Wong <ivanwong810520@gmail.com>
2026-04-17 19:53:33 +0800

    Add skills and badges section

--- ui/features/work/index.ts
+++ ui/features/work/index.ts
@@ -1,2 +1,6 @@
 export { default as WorkTimeline } from "./WorkTimeline";
 export { default as WorkTimelineSkeleton } from "./WorkTimelineSkeleton";
+export { default as WorkSkills } from "./WorkSkills";
+export { default as WorkSkillsSkeleton } from "./WorkSkillsSkeleton";
+export { default as WorkBadges } from "./WorkBadges";
+export { default as WorkBadgesSkeleton } from "./WorkBadgesSkeleton";
